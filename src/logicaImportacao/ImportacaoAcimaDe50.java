0cd4007c0b0c9d13b2ba50970306a00d15f589de
Gabi <gabimelo640@gmail.com>
2025-10-04 15:31:37 -0300

    Implementaçao da lógica da importação

--- src/logicaImportacao/ImportacaoAcimaDe50.java
+++ src/logicaImportacao/ImportacaoAcimaDe50.java
@@ -3,7 +3,7 @@ package logicaImportacao;
 public class ImportacaoAcimaDe50 implements Imposto{
     @Override
     public double calcularImposto(double valor) {
- //Se o valor da compra for de até 50 dólares (ou o equivalente): o imposto de importação será de 20% sobre esse valor
-
+// Se o valor da compra* for acima de 50 dólares: o Imposto de Importação será de 60% esse valor.
+        return valor + (0.5 * valor);
     }
 }

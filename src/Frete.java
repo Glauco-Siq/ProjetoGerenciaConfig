06438aec7f13c90fe4101707c93cec3c420f7d46
Glauco Siqueira <glauco0909@gmail.com>
2025-10-04 15:14:02 -0300

    mudanca na interface - mudei assinatura de void pra double

--- src/Frete.java
+++ src/Frete.java
@@ -1,3 +1,3 @@
 public interface Frete {
-    public void calcularFrete();
+    public double calcularFrete();
 }
--- src/Imposto.java
+++ src/Imposto.java
@@ -1,3 +1,3 @@
 public interface Imposto {
-    public void calcularImposto();
+    public double calcularImposto();
 }

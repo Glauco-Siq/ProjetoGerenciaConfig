d4aedeb56a2d32684d2c552a8807fb666b56ed26
Glauco Siqueira <glauco0909@gmail.com>
2025-10-04 15:16:37 -0300

    mudanca na interface - mudei assinatura de void pra double

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int escolhaFrete = sc.nextInt();
-
-        System.out.println("----------------------------------");
-        System.out.println("Bem vindo à calculadora de fretes!");
-        System.out.println("");
-        System.out.println("Qual tipo de frete você deseja escolher?");
-        System.out.println("1 - Normal");
-        System.out.println("2 - Expresso");
-        System.out.println("3 - SuperExpresso");
-
-
-
     }
 }
